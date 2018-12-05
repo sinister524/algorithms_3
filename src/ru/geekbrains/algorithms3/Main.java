cfcf3c69c171114ebae0788f6a06cff5348b8949
sinister524 <sinister524@gmail.com>
2018-12-05 23:34:02 +0300

    Подготовил домашнее задане

--- src/ru/geekbrains/algorithms3/Main.java
+++ src/ru/geekbrains/algorithms3/Main.java
@@ -1,8 +1,14 @@
 package ru.geekbrains.algorithms3;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введите слово:");
+        String str = scanner.next();
+        Words.revers(str);
     }
 }
