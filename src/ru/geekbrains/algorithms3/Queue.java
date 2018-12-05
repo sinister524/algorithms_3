61afdbf55c9165d4988857bbcaa5f2d68644d0d3
sinister524 <sinister524@gmail.com>
2018-12-06 00:05:54 +0300

    Подготовил домашнее задание

--- src/ru/geekbrains/algorithms3/Queue.java
+++ src/ru/geekbrains/algorithms3/Queue.java
@@ -3,15 +3,15 @@ package ru.geekbrains.algorithms3;
 public class Queue {
     private int maxSize;
     private int[] queue;
-    private int head;
-    private int tail;
+    private int front;
+    private int rear;
     private int items;
 
     public Queue(int size) {
         this.maxSize = size;
         this.queue = new int[size];
-        this.head = 0;
-        this.tail = -1;
+        this.front = 0;
+        this.rear = -1;
         this.items = 0;
     }
 
@@ -31,34 +31,34 @@ public class Queue {
         if (isFull()) {
             maxSize *=2;
             int[] temp = new int[maxSize];
-            if (tail >= head){
+            if (rear >= front){
                 System.arraycopy(queue, 0, temp, 0, queue.length);
             } else {
-                System.arraycopy(queue, 0, temp, 0, tail + 1);
-                System.arraycopy(queue, head, temp,
-                        size() - (queue.length - head),
-                        queue.length - head);
-                head = maxSize - head - 1;
+                System.arraycopy(queue, 0, temp, 0, rear + 1);
+                System.arraycopy(queue, front, temp,
+                        size() - (queue.length - front),
+                        queue.length - front);
+                front = maxSize - front - 1;
             }
             queue = temp;
         }
 
-        if (tail == maxSize - 1)
-            tail = -1;
-        queue[++tail] = i;
+        if (rear == maxSize - 1)
+            rear = -1;
+        queue[++rear] = i;
         items++;
     }
 
     public int remove(){
         if (isEmpty()) throw new RuntimeException("Queue is empty");
-        int temp = queue[head++];
-        if (head == maxSize)
-            head = 0;
+        int temp = queue[front++];
+        if (front == maxSize)
+            front = 0;
         items--;
         return temp;
     }
 
     public int peekFront(){
-        return queue[head];
+        return queue[front];
     }
 }
